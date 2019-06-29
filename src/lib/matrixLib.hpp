9061da6700c5fc5273d1da6f8c5d4e22ecf62a39
Jippe Heijnen <jippe.heijnen@student.hu.nl>
2019-06-29 19:53:23 +0200

    Updated the files, seperated library files. (sorta) working hourglass now.

--- src/lib/matrixLib.hpp
+++ src/lib/matrixLib.hpp
@@ -40,11 +40,7 @@ namespace spi {
 			\param mosi This is the mosi (data) pin.
 			\param cs This is the cs pin.
 		*/
-		bus(hwlib::target::pin_out &clk, hwlib::target::pin_out &mosi, hwlib::target::pin_out &cs):
-			clk(hwlib::pin_direct_from_out_t(clk)),
-			mosi(hwlib::pin_direct_from_out_t(mosi)),
-			cs(hwlib::pin_direct_from_out_t(cs))
-			{}
+		bus(hwlib::target::pin_out &clk, hwlib::target::pin_out &mosi, hwlib::target::pin_out &cs);
 	};
 
 	//! SPI transaction class.
@@ -60,23 +56,11 @@ namespace spi {
 		hwlib::pin_direct_from_out_t &cs;
 		friend class bus;
 	public:
-		transaction(spi::bus &b) :
-			b(b), clk(b.clk), mosi(b.mosi), cs(b.cs)
-		{
-			cs.write(0);  // start of transmission
-		}
+		transaction(spi::bus &b);
 
-		void writeData(uint8_t bits, uint16_t d) {
-			for (uint16_t bit = 1<<(bits-1); bit; bit >>= 1) {
-				clk.write(0);
-				mosi.write((d & bit) ? 1 : 0);
-				clk.write(1);
-			}
-		}
+		void writeData(uint8_t bits, uint16_t d);
 
-		~transaction() {
-			cs.write(1);  // end of transmission
-		}
+		~transaction();
 
 	};
 
@@ -94,9 +78,11 @@ namespace matrix {
 
 struct commands {
 
-static constexpr const uint8_t HT1632_READ			= 0x06;
+/*!
+	\addtogroup commands
+	@{
+*/
 
-static constexpr const uint8_t HT1632_WRITE			= 0x05;
 static constexpr const uint8_t HT1632_COMMAND		= 0x04;
 
 static constexpr const uint8_t HT1632_SYS_DIS		= 0x00;
@@ -116,134 +102,48 @@ static constexpr const uint8_t HT1632_COMMON_16NMOS	= 0x24;
 static constexpr const uint8_t HT1632_COMMON_8PMOS	= 0x28;
 static constexpr const uint8_t HT1632_COMMON_16PMOS	= 0x2C;
 
+static constexpr const uint8_t HT1632_READ			= 0x06;
+static constexpr const uint8_t HT1632_WRITE			= 0x05;
+
+/*! @} End of Doxygen Groups*/
+
 };
 
 
 class HT_1632 {
 protected:
-	spi::bus spi_bus;
 
-void sendCommand(uint16_t cmd){
-	spi::transaction(spi_bus).writeData(12, (((uint16_t)commands::HT1632_COMMAND << 8) | cmd) << 1);
-}
-
-void writeRam(uint8_t addr, uint8_t data) {
-
-	uint16_t d = commands::HT1632_WRITE;
-	d <<= 7;
-	d |= addr & 0x7F;
-	d <<= 4;
-	d |= data & 0xF;
-	spi::transaction(spi_bus).writeData(14, d);
-}
-
+uint16_t ledmatrix[24] = {0};
+spi::bus spi_bus;
+uint16_t type;
+void sendCommand(uint16_t cmd);
+void writeRam(uint8_t addr, uint8_t data);
+void dumpMem();
+friend class matrixWindow;
 
 public:
-	uint16_t ledmatrix[24] = {0};
-	uint16_t type;
 
-HT_1632(spi::bus spi_bus,
-	uint16_t type):
-	spi_bus(spi_bus),
-	type(type)
-	{}
-
-void begin() {
-
-	sendCommand(commands::HT1632_SYS_EN);
-	sendCommand(commands::HT1632_LED_ON);
-	sendCommand(commands::HT1632_BLINK_OFF);
-	sendCommand(commands::HT1632_MASTER_MODE);
-	sendCommand(commands::HT1632_INT_RC);
-	sendCommand(type);
-	sendCommand(commands::HT1632_PWM_CONTROL | 0xf);
-
-	for (int i=0;i<24;i++) {
-		ledmatrix[i]=0;
-	}
-
-	for (int i=0; i<24*4; i++) {
-		writeRam(i, 0x00);
-	}
-
-}
-
-
-// set to true for memory debugging
-#define debug false
-
-void dumpMem() {
-	if (debug) {
-		hwlib::wait_ms(10);
-		for (uint16_t i=0; i<24;i++) {
-			if(i%2 == 0)
-				hwlib::cout << '\n' ;
-			hwlib::cout << hwlib::bin << ledmatrix[i] << '|' << hwlib::flush;
-			hwlib::wait_ms(5);
-		}
-		hwlib::cout << '\n';
-	} else {
-		for (unsigned int i=0; i<24; i++) {
-			ledmatrix[i] = 0x00;
-		}
-		hwlib::cout << "memory cleared\n";
-	}
-}
-
-void setPixel(uint16_t x, uint16_t y) {
-
-	if((x < 0) || (x >= 16) || (y < 0) || (y >= 24)) return;
-	ledmatrix[y] |= 0x8000 >> x;
-
-}
-
-void clearPixel(uint16_t x, uint16_t y) {
-
-	if((x < 0) || (x >= 16) || (y < 0) || (y >= 24)) return;
-	ledmatrix[y] &= ~(0x8000 >> x);
-
-}
-
-void writeScreen() {
-	auto spi_transaction = spi::transaction(spi_bus);
-	spi_transaction.writeData(3, commands::HT1632_WRITE);
-	spi_transaction.writeData(7,0);
-
-	for(uint16_t i=0; i<24; i++) {
-		spi_transaction.writeData(16, ledmatrix[i]);
-	}
-	spi_transaction.~transaction();
-
-}
-
-void setBrightness(uint8_t b) {
-	sendCommand(commands::HT1632_PWM_CONTROL | b);
-}
+HT_1632(spi::bus spi_bus, uint16_t type);
+void setPixel(uint16_t x, uint16_t y);
+void clearPixel(uint16_t x, uint16_t y);
+void blink(uint16_t seconds);
+void writeScreen();
+void setBrightness(uint8_t b);
 
 };
 
 class matrixWindow : public hwlib::window {
-	// TODO: merge matrixWindow with ht_1632 class.
+
 private:
+
 	HT_1632 &matrix;
+
 public:
-	matrixWindow(int x, int y, HT_1632 &matrix) :
-		hwlib::window(hwlib::xy(x,y)),
-		matrix(matrix)
-		{}
 
-	void write_implementation(hwlib::xy pos, hwlib::color col) override {
-		matrix.setPixel(pos.x, pos.y);
-	}
-
-	void flush() override {
-		matrix.writeScreen();
-	}
-
-	void clear()  {
-		matrix.dumpMem();
-		matrix.writeScreen();
-	}
+	matrixWindow(int x, int y, HT_1632 &matrix);
+	void write_implementation(hwlib::xy pos, hwlib::color col) override;
+	void flush() override;
+	void clear();
 
 };
 
--- test/iPass_test/main.cpp
+++ test/iPass_test/main.cpp
@@ -8,278 +8,220 @@ using matrix::commands, matrix::HT_1632, matrix::matrixWindow;
 class drawable {
 protected:
 
-   hwlib::window & w;
-   hwlib::xy location;
-   hwlib::xy size;
+	hwlib::window & w;
 
 public:
+	hwlib::xy location;
+	hwlib::xy size;
 
-   hwlib::xy bounce;
-   drawable( hwlib::window & w, const hwlib::xy & location, const hwlib::xy & size, const hwlib::xy bounce ):
-      w( w ),
-      location( location ),
-      size( size ),
-	  bounce( bounce )
-   {}
+	hwlib::xy bounce;
+	drawable( hwlib::window & w, const hwlib::xy & location, const hwlib::xy & size, const hwlib::xy bounce ):
+		w( w ),
+		location( location ),
+		size( size ),
+		bounce( bounce )
+	{}
 
-   virtual void draw() = 0;
-   virtual void update(){}
-   bool overlaps( const drawable & other );
-   virtual void interact( drawable & other ){}
+	virtual void draw() = 0;
+	virtual void update(){}
+	bool overlaps( const drawable & other );
+	virtual void interact( drawable & other ){}
 
-   hwlib::ostream & print( hwlib::ostream & out ) const {
-      return out << location << " " << ( location + size );
-   }
+	hwlib::ostream & print( hwlib::ostream & out ) const {
+		return out << location << " " << ( location + size );
+	}
 };
 
 hwlib::ostream & operator<<( hwlib::ostream & lhs, const drawable & rhs ){
-   return rhs.print( lhs );
+	return rhs.print( lhs );
 }
 
 bool within( int x, int a, int b ){
-   return ( x >= a+1 ) && ( x <= b+1 );
+	return ( x >= a+1 ) && ( x <= b+1 );
 }
 
 bool drawable::overlaps( const drawable & other ){
 
-   bool x_overlap = within(
-      location.x,
-      other.location.x,
-      other.location.x + other.size.x
-   ) || within(
-      other.location.x,
-      location.x,
-      location.x + size.x
-   );
+	bool x_overlap = within(
+		location.x,
+		other.location.x,
+		other.location.x + other.size.x
+	) || within(
+		other.location.x,
+		location.x,
+		location.x + size.x
+	);
 
-   bool y_overlap = within(
-      location.y,
-      other.location.y,
-      other.location.y + other.size.y
-   ) || within(
-      other.location.y,
-      location.y,
-      location.y + size.y
-   );
+	bool y_overlap = within(
+		location.y,
+		other.location.y,
+		other.location.y + other.size.y
+	) || within(
+		other.location.y,
+		location.y,
+		location.y + size.y
+	);
 
-   return x_overlap && y_overlap;
+	return x_overlap && y_overlap;
 }
 
 // ===========================================================================
 
 class line : public drawable {
 private:
-   hwlib::xy end;
-   hwlib::xy bounce = hwlib::xy(-1,-1);
 public:
+	hwlib::xy end;
+	hwlib::xy bounce;
 
-   line( hwlib::window & w, const hwlib::xy & location, const hwlib::xy & end, const hwlib::xy &bounce ):
-      drawable( w, location, end - location, bounce ),
-      end( end )
-   {}
-
-   void draw() override {
-      hwlib::line x( location, end );
-      x.draw( w );;
-   }
-};
-
-// ===========================================================================
-
-class circle : public drawable {
-protected:
-
-   int radius;
-
-public:
-
-   circle( hwlib::window & w, const hwlib::xy & midpoint, int radius, const hwlib::xy &bounce ):
-      drawable( w,
-         midpoint - hwlib::xy( radius, radius ),
-         hwlib::xy( radius, radius ) * 2, bounce ),
-      radius( radius )
-   {}
-
-   void draw() override {
-      hwlib::circle c( location + hwlib::xy( radius, radius ), radius );
-      c.draw( w );
-   }
-};
-
-// ===========================================================================
-
-class ball : public circle {
-private:
-<<<<<<< HEAD
-
-   hwlib::xy speed;
-
-public:
-
-   ball(
-      hwlib::window & w,
-      const hwlib::xy & midpoint,
-      int radius = 0,
-      const hwlib::xy & speed = hwlib::xy(0,1),
-	  const hwlib::xy &bounce = hwlib::xy(1,1)
-   ):
-      circle( w, midpoint, radius, bounce ),
-      speed( speed )
-   {}
-
-   void draw() override {
-	   w.write(hwlib::xy(location.x-1, location.y-1));
-   }
-
-   void update() override {
-      location = location + speed;
-   }
-
-   void interact( drawable & other ) override {
-      if( this != & other){
-         if( overlaps( other )){
-            speed.x *= other.bounce.x;
-            speed.y *= other.bounce.y;
-         }
-      }
-   }
-};
-
-// ===========================================================================
-
-class rectangle : public drawable {
-protected:
-	hwlib::color color;
-
-public:
-	rectangle(hwlib::window &w, const hwlib::xy &location, const hwlib::xy &size, hwlib::color color, const hwlib::xy &bounce) :
-		drawable(w, location, size, bounce),
-		color(color)
-		{}
-};
-
-// ===========================================================================
-
-class wall : public rectangle {
-private:
-	int update_count = 0,
-		update_interval;
-	bool filled = false;
-
-public:
-	wall(hwlib::window &w, const hwlib::xy &location, const hwlib::xy &size, int update_interval=0, hwlib::color color=hwlib::red, const hwlib::xy &bounce=hwlib::xy(0,-1)) :
-		rectangle(w, location, size, color, bounce),
-		update_interval(update_interval)
-		{}
-
-=======
-	
-	hwlib::xy speed;
-	
-public:
-	
-	ball( 
-		hwlib::window & w, 
-		const hwlib::xy & midpoint, 
-		int radius = 0, 
-		const hwlib::xy & speed = hwlib::xy(0,1),
-		const hwlib::xy &bounce = hwlib::xy(1,1)
-	):
-		circle( w, midpoint, radius, bounce ),
-		speed( speed )  
+	line( hwlib::window & w, const hwlib::xy & location, const hwlib::xy & end, const hwlib::xy &bounce  = hwlib::xy(-1,-1)):
+		drawable( w, location, end - location, bounce ),
+		end( end )
 	{}
-	
->>>>>>> 4b28ba6a65e01e9a92b38610ea73ebb5a20b42aa
+
 	void draw() override {
-		w.write(hwlib::xy(location.x-1, location.y-1));
-	}
-<<<<<<< HEAD
-
-	void update() override {
-		update_count++;
-		if (update_count==update_interval) {
-			filled = !(filled);
-			update_count=0;
-		}
-
+		hwlib::line x( location, end );
+		x.draw( w );;
 	}
 
-	void drawHollow() {
-		for(int i = location.x; i <= (location.x + size.x); i++){
-            for(int j = location.y; j <= (location.y + size.y); j++){
-				hwlib::xy pixel(i,j);
-				if(j == location.y || j == (location.y + size.y) || i == location.x || i == (location.x +size.x)){
-                    w.write(pixel, hwlib::color(0,0,0,false));
-                }else if(filled){
-                    w.write(pixel, hwlib::color(255,0,0,false));
-                }
-			}
-		}
-		w.flush();
-	}
-
-	void drawFilled() {
-		for(int i = location.x; i <= (location.x + size.x); i++){
-            for(int j = location.y; j <= (location.y + size.y); j++){
-				hwlib::xy pixel(i,j);
-				if(j == location.y || j == (location.y + size.y) || i == location.x || i == (location.x +size.x)){
-                    w.write(pixel, hwlib::color(0,0,0,false));
-                }else if(filled){
-                    w.write(pixel, hwlib::color(255,0,0,false));
-                }
-			}
-		}
-		w.flush();
-	}
-
-=======
-   
-	void update() override {
-		speed = hwlib::xy(0, 1);
-		location = location + speed; 
-	}
-	
-	void interact( drawable & other ) override {
-		if( this != & other){
-			if( overlaps( other )){
-				speed.x *= other.bounce.x;
-				speed.y *= other.bounce.y;
-			}
-		}
-	}   
->>>>>>> 4b28ba6a65e01e9a92b38610ea73ebb5a20b42aa
 };
 
 // ===========================================================================
 
 
-void hourGlass(matrix::HT_1632 &m) {
-	auto win = matrixWindow(16, 24, m);
-	line(win, hwlib::xy(1,5), hwlib::xy(6,10), hwlib::xy(1,0)).draw();
-	line(win, hwlib::xy(15,4), hwlib::xy(9,10), hwlib::xy(0,0)).draw();
-	line(win, hwlib::xy(0,19), hwlib::xy(7,12), hwlib::xy(0,0)).draw();
-	line(win, hwlib::xy(15,19), hwlib::xy(8,12), hwlib::xy(0,0)).draw();
 
-	line(win, hwlib::xy(0,0), hwlib::xy(0,5), hwlib::xy(0,1)).draw();
-	line(win, hwlib::xy(0,19), hwlib::xy(0,24), hwlib::xy(0,0)).draw();
-	line(win, hwlib::xy(15,0), hwlib::xy(15,4), hwlib::xy(0,0)).draw();
-	line(win, hwlib::xy(15,19), hwlib::xy(15,24), hwlib::xy(0,0)).draw();
-	line(win, hwlib::xy(6,10), hwlib::xy(6,14), hwlib::xy(0,0)).draw();
-	line(win, hwlib::xy(9,10), hwlib::xy(9,14), hwlib::xy(0,1)).draw();
+void hourGlass(matrix::HT_1632 &matrix, uint16_t h, uint16_t m, uint16_t s) {
+	auto time = (h*3600)+(m*60)+s;
 
-	line(win, hwlib::xy(0,0), hwlib::xy(15,0), hwlib::xy(0,0)).draw();
-	line(win, hwlib::xy(0,23), hwlib::xy(15,23), hwlib::xy(0,0)).draw();
-	
+	auto w = matrix::matrixWindow(16, 24, matrix);
 
-	m.clearPixel(0,0);
-	m.clearPixel(15,0);
-	m.clearPixel(0,23);
-	m.clearPixel(15,23);
+	auto l0 = line(w, hwlib::xy(2,2), hwlib::xy(14,2));
+	auto l1 = line(w, hwlib::xy(2,3), hwlib::xy(14,3));
+	auto l2 = line(w, hwlib::xy(3,4), hwlib::xy(13,4));
+	auto l3 = line(w, hwlib::xy(4,5), hwlib::xy(12,5));
+	auto l4 = line(w, hwlib::xy(5,6), hwlib::xy(11,6));
+	auto l5 = line(w, hwlib::xy(6,7), hwlib::xy(10,7));
+	auto l6 = line(w, hwlib::xy(7,8), hwlib::xy(9,8));
+
+	std::array< drawable *, 7 > lines = {
+		&l0, &l1, &l2, &l3, &l4, &l5, &l6
+	};
+
+
+	bool makeItRain = true;
+	int rainCounter = 0;
 	
-	m.writeScreen();
-	
+	for (float counter=0;counter!=time; counter+=.25) {
+		w.clear();
+		line(w, hwlib::xy(1,5), hwlib::xy(6,10)).draw();
+		line(w, hwlib::xy(15,4), hwlib::xy(9,10)).draw();
+		line(w, hwlib::xy(0,19), hwlib::xy(7,12)).draw();
+		line(w, hwlib::xy(15,19), hwlib::xy(8,12)).draw();
+
+		line(w, hwlib::xy(0,0), hwlib::xy(0,5)).draw();
+		line(w, hwlib::xy(0,19), hwlib::xy(0,24)).draw();
+		line(w, hwlib::xy(15,0), hwlib::xy(15,4)).draw();
+		line(w, hwlib::xy(15,19), hwlib::xy(15,24)).draw();
+		line(w, hwlib::xy(6,10), hwlib::xy(6,14)).draw();
+		line(w, hwlib::xy(9,10), hwlib::xy(9,14)).draw();
+
+		line(w, hwlib::xy(0,0), hwlib::xy(15,0)).draw();
+		line(w, hwlib::xy(0,23), hwlib::xy(15,23)).draw();
+
+		for (auto &l : lines) {
+			l->draw();
+		}
+
+		w.flush();
+
+		matrix.clearPixel(0,0);
+		matrix.clearPixel(15,0);
+		matrix.clearPixel(0,23);
+		matrix.clearPixel(15,23);
+		matrix.writeScreen();
+		hwlib::wait_ms(250);
+		if (makeItRain) {
+			rainCounter++;
+			w.flush();
+		}
+		if (counter>=7*(time/7)) {
+			hwlib::cout << "7/7" << '\n';
+			l6.location=hwlib::xy(7, 15);
+			l6.end=hwlib::xy(9, 15);
+			l5.location=hwlib::xy(6, 16);
+			l5.end=hwlib::xy(10, 16);
+			l4.location=hwlib::xy(5, 17);
+			l4.end=hwlib::xy(11, 17);
+			l3.location=hwlib::xy(4, 18);
+			l3.end=hwlib::xy(12, 18);
+			l2.location=hwlib::xy(3, 19);
+			l2.end=hwlib::xy(13, 19);
+			l1.location=hwlib::xy(2, 20);
+			l1.end=hwlib::xy(14, 20);
+			l0.location=hwlib::xy(2, 21);
+			l0.end=hwlib::xy(14, 21);
+		} else if (counter>=6*(time/7)) {
+			makeItRain = false;
+			hwlib::cout << "6/7" << '\n';
+			l5.location=hwlib::xy(6, 16);
+			l5.end=hwlib::xy(10, 16);
+			l4.location=hwlib::xy(5, 17);
+			l4.end=hwlib::xy(11, 17);
+			l3.location=hwlib::xy(4, 18);
+			l3.end=hwlib::xy(12, 18);
+			l2.location=hwlib::xy(3, 19);
+			l2.end=hwlib::xy(13, 19);
+			l1.location=hwlib::xy(2, 20);
+			l1.end=hwlib::xy(14, 20);
+			l0.location=hwlib::xy(2, 21);
+			l0.end=hwlib::xy(14, 21);
+		} else if (counter>=5*(time/7)) {
+			hwlib::cout << "5/7" << '\n';
+			l4.location=hwlib::xy(5, 17);
+			l4.end=hwlib::xy(11, 17);
+			l3.location=hwlib::xy(4, 18);
+			l3.end=hwlib::xy(12, 18);
+			l2.location=hwlib::xy(3, 19);
+			l2.end=hwlib::xy(13, 19);
+			l1.location=hwlib::xy(2, 20);
+			l1.end=hwlib::xy(14, 20);
+			l0.location=hwlib::xy(2, 21);
+			l0.end=hwlib::xy(14, 21);
+		} else if (counter>=4*(time/7)) {
+			hwlib::cout << "4/7" << '\n';
+			l3.location=hwlib::xy(4, 18);
+			l3.end=hwlib::xy(12, 18);
+			l2.location=hwlib::xy(3, 19);
+			l2.end=hwlib::xy(13, 19);
+			l1.location=hwlib::xy(2, 20);
+			l1.end=hwlib::xy(14, 20);
+			l0.location=hwlib::xy(2, 21);
+			l0.end=hwlib::xy(14, 21);
+		} else if (counter>=3*(time/7)) {
+			hwlib::cout << "3/7" << '\n';
+			l2.location=hwlib::xy(3, 19);
+			l2.end=hwlib::xy(13, 19);
+			l1.location=hwlib::xy(2, 20);
+			l1.end=hwlib::xy(14, 20);
+			l0.location=hwlib::xy(2, 21);
+			l0.end=hwlib::xy(14, 21);
+		} else if (counter>=2*(time/7)) {
+			hwlib::cout << "2/7" << '\n';
+			l1.location=hwlib::xy(2, 20);
+			l1.end=hwlib::xy(14, 20);
+			l0.location=hwlib::xy(2, 21);
+			l0.end=hwlib::xy(14, 21);
+		} else if (counter>=(time/7)) {
+			hwlib::cout << "1/7" << '\n';
+			l0.location=hwlib::xy(2, 21);
+			l0.end=hwlib::xy(14, 21);
+		}
+	}
+	matrix.blink(4);
+	hwlib::cout << "done";
+
 }
 
+// ===========================================================================
 
 int main(){
 
@@ -288,72 +230,14 @@ int main(){
 	auto cs = hwlib::target::pin_out(hwlib::target::pins::d8);
 	auto spi_bus = spi::bus(wr, dat, cs);
 	auto m = HT_1632(spi_bus, matrix::commands::HT1632_COMMON_16NMOS);
-	
-	m.begin();
+
 	m.setBrightness(0x0);
 	hwlib::wait_ms(500);
-<<<<<<< HEAD
 
 	auto win = matrix::matrixWindow(16, 24, m);
-	auto l0 = line(win, hwlib::xy(0,4), hwlib::xy(6,10), hwlib::xy(0,0));
-	auto l1 = line(win, hwlib::xy(15,4), hwlib::xy(9,10), hwlib::xy(0,0));
-	auto l2 = line(win, hwlib::xy(0,19), hwlib::xy(7,12), hwlib::xy(0,0));
-	auto l3 = line(win, hwlib::xy(15,19), hwlib::xy(8,12), hwlib::xy(0,0));
-
-	auto l00 = line(win, hwlib::xy(0,0), hwlib::xy(0,4), hwlib::xy(0,0));
-	auto l01 = line(win, hwlib::xy(0,19), hwlib::xy(0,24), hwlib::xy(0,0));
-	auto l02 = line(win, hwlib::xy(15,0), hwlib::xy(15,4), hwlib::xy(0,0));
-	auto l03 = line(win, hwlib::xy(15,19), hwlib::xy(15,24), hwlib::xy(0,0));
-	auto l04 = line(win, hwlib::xy(6,10), hwlib::xy(6,14), hwlib::xy(0,0));
-	auto l05 = line(win, hwlib::xy(9,10), hwlib::xy(9,14), hwlib::xy(0,1));
-
-	auto l000 = line(win, hwlib::xy(0,0), hwlib::xy(15,0), hwlib::xy(0,0));
-	auto l001 = line(win, hwlib::xy(0,23), hwlib::xy(15,23), hwlib::xy(0,0));
-
-	std::array< drawable *, 18 > objects = { &l0, &l1, &l2, &l3, &l00, &l01, &l02, &l03, &l04, &l05, &l000, &l001 };
+	hourGlass(m, 0, 0, 10);
 
 
-
-	for (;;) {
-		for( auto & p : objects ){
-			p->draw();
-		}
-		for( auto & p : objects ){
-			p->update();
-		}
-		for( auto & p : objects ){
-			for( auto & other : objects ){
-				p->interact( *other );
-			}
-		}
-
-		m.writeScreen();
-		m.clearPixel(0,0);
-		m.clearPixel(15,0);
-		m.clearPixel(0,23);
-		m.clearPixel(15,23);
-		m.writeScreen();
-
-		hwlib::wait_ms(100);
-		win.clear();
-		win.flush();
-	}
-=======
-	
-	int i=0;
-	for (int x=2; x<14; x++ ) {
-		for (int y=2; y<9; y++) {
-			m.setPixel(x,y);
-			m.clearPixel(2+i,y);
-			m.clearPixel(13-i,y);
-		}
-		i++;
-	}
-	
-	
-	hourGlass(m);
-	
-	m.writeScreen();
-
->>>>>>> 4b28ba6a65e01e9a92b38610ea73ebb5a20b42aa
 }
+
+
